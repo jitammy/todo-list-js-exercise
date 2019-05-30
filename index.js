cb605f7e0b8877954c26dd3b5b224194935a94e9
jitammy <jitammy@gmail.com>
2019-05-30 17:52:08 +0000

    refactoring for modularity

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
+// const taskTitles = [];
+// const taskComplete = [];
+// const taskDescriptions = [];
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
 // function newTask(title, description) {
@@ -9,19 +9,35 @@ const taskDescriptions = [];
 //   taskDescriptions.push(description)
 //   taskComplete.push(false);
 // }
-function newTask (title, description) {
-  const task = {
-    title: title,
-    description: description,
-    complete: false,
-    logState: function(){
-       console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-    },
-    markCompleted: function(){
+// function newTask (title, description) {
+
+  
+//   const task = {
+//     title: title,
+//     description: description,
+//     complete: false,
+//     logState: function(){
+//        console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+//     },
+//     markCompleted: function(){
+//       this.complete = true
+//     }
+//   }
+//   return task
+// }
+
+class Task {
+  constructor (title, description){
+    this.title = title
+    this.description = description
+    this.complete = false
+    this.logState = function (){
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`)
+    }
+    this.markCompleted = function (){
       this.complete = true
     }
   }
-  return task
 }
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 // function completeTask(taskIndex) {
@@ -37,14 +53,17 @@ function newTask (title, description) {
 // DRIVER CODE BELOW
 // newTask("Clean Cat Litter"); // task 0
 // newTask("Do Laundry"); // task 1
-const task1 = newTask('clean cat litter', 'take all the shit out of the litter box')
-const task2 = newTask ('Do laundry', 'haha')
-const tasks = [task1, task2]
-console.log(tasks)
-task1.logState()
+let task1 = new Task("clean cat litter", "take all the shit out of the litter box")
+let task2 = new Task ('Do laundry', 'haha')
+// console.log(task1)
+// console.log(task2)
+
+let tasks = [task1, task2]
+// task1.logState()
 task1.markCompleted()
-task1.logState()
-task1.title
+// task1.logState()
+// task1.title
+console.log(tasks)
 
 // logTaskState(0); // Clean Cat Litter has not been completed
 // completeTask(0);
